60e9c83e686ff41b94f2c4f89dfedcfd6b0b04c1
Paul Graydon <paulgraydon@hotmail.com>
2024-12-06 09:16:47 -0800

    Day 4, Part 2

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -78,6 +78,66 @@ fn part_one(input: &Vec<Vec<char>>) {
 
 fn part_two(input: &Vec<Vec<char>>) {
     let start = std::time::Instant::now();
+    let mut count: u32 = 0;
+    // x is rows, y is columns
+    for x in 0..input.len() {
+        for y in 0..input[x].len() {
+            if x + 2 < input.len() && y + 2 < input[x].len() {
+                // Check the middle for quick elimination
+                if input[x + 1][y + 1] == 'A' {
+                    // "MAS" left to right
+                    // Top left
+                    if input[x][y] == 'M'
+                    // Bottom Left
+                    && input[x+2][y] == 'M'
+                    // Bottom Right
+                    && input[x+2][y+2] == 'S'
+                    // Top Right
+                    && input[x][y+2] == 'S'
+                    {
+                        count += 1;
+                    }
+                    // "SAM" left to right
+                    // Top left
+                    if input[x][y] == 'S'
+                    // Bottom Left
+                    && input[x+2][y] == 'S'
+                    // Bottom Right
+                    && input[x+2][y+2] == 'M'
+                    // Top Right
+                    && input[x][y+2] == 'M'
+                    {
+                        count += 1;
+                    }
+                    // "MAS" top left, to bottom right. "SAM" bottom left to top right
+                    // Top left
+                    if input[x][y] == 'M'
+                    // Bottom Left
+                    && input[x+2][y] == 'S'
+                    // Bottom Right
+                    && input[x+2][y+2] == 'S'
+                    // Top Right
+                    && input[x][y+2] == 'M'
+                    {
+                        count += 1;
+                    }
+                    // "SAM" top left, to bottom right. "MAS" bottom left to top right
+                    // Top left
+                    if input[x][y] == 'S'
+                    // Bottom Left
+                    && input[x+2][y] == 'M'
+                    // Bottom Right
+                    && input[x+2][y+2] == 'M'
+                    // Top Right
+                    && input[x][y+2] == 'S'
+                    {
+                        count += 1;
+                    }
+                }
+            }
+        }
+    }
+    println!("Answer: {count}");
     println!("Part two time taken: {:?}", start.elapsed());
 }
 
